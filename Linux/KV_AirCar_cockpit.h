6ef3380e388c60c1ccc6203df5b941fa0f4ce7a2
MatiasSaibene <matiassaibene98@gmail.com>
2025-11-15 22:54:04 -0300

    Added Readme.md and Linux sources

---
+++ Linux/KV_AirCar_cockpit.h
@@ -0,0 +1,23 @@
+// Auto generated code file.  Blender: 3.3.9  Blender Tools: (2, 1, 3)
+// Date: Thu Jan 30 22:21:10 2025
+
+
+#include "Orbitersdk.h"
+
+#ifndef __KV_AirCar_Cockpit_H
+#define __KV_AirCar_Cockpit_H
+
+
+    const UINT Cockpit_panel_Id = 0;
+    const UINT Cockpit_seats_Id = 1;
+    const UINT MFD1_Id = 2;
+    const UINT MFD2_Id = 3;
+    constexpr VECTOR3 Cockpit_panel_Location =     {-0.0002, 0.8840, 1.8548};
+    constexpr VECTOR3 Cockpit_seats_Location =     {-0.0067, 0.6524, 1.3479};
+    constexpr VECTOR3 VC_camera1_Location =     {-0.4000, 1.1000, 1.1000};
+    constexpr VECTOR3 VC_camera2_Location =     {0.4000, 1.1000, 1.1000};
+    constexpr VECTOR3 MFD1_Location =     {-0.0002, 0.8840, 1.8548};
+    constexpr VECTOR3 MFD2_Location =     {-0.0002, 0.8840, 1.8548};
+
+
+#endif
---
+++ README.md
@@ -0,0 +1,10 @@
+**
+
+## KleinVision AirCar for OpenOrbiter (Linux and Windows)
+
+How to install:
+Simply drag the contents (that is, the Config, Images, Meshes, Modules, Scenarios, Orbitersdk and Textures folders) from the folder that corresponds to your operating system to the main Orbiter folder.
+
+If you liked it, you can leave me a comment in the Orbiter Forum.
+
+KleinVision AirCar for OpenOrbiter is licensed under the terms of the MIT license. You can find the source code inside the Orbitersdk/samples/KleinVision_AirCar folder.
